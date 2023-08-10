bb9340af03f6d6a1c0fac7e6a3f35fedbc048d23
Ramesh Basavraj Khed <rameshkhed3298@gmail.com>
2023-08-10 22:00:14 +0530

    the game is ready

---
+++ com/myGame/Main.java
@@ -0,0 +1,12 @@
+package com.myGame;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Tic tac toe game");
+
+        GameGUI game = new GameGUI();
+
+
+        
+    }
+}
